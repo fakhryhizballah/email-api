5d803b63d8adb237c9b451983b9378aa0385eafb
fakhryhizballah <falehry88@gmail.com>
2023-09-03 14:45:57 +0700

    add auth

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const base = require('../controllers');
 const middleware = require('../middlewares');
 
 
-routes.post('/email/send', base.sendEmail);
+routes.post('/email/send', middleware.login, base.sendEmail);
 
 
 module.exports = routes;
