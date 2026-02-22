206b34e4192020c2ab590f90c7df84ee21fd511f
Fakhry Hizballah <50532299+fakhryhizballah@users.noreply.github.com>
2026-02-22 21:06:49 +0700

    Enable secure SMTP connection and enforce TLS

--- controllers/index.js
+++ controllers/index.js
@@ -6,13 +6,13 @@ const fs = require('fs');
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
     tls: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: true,
         // minVersion: "TLSv1.2"
     }
 });
